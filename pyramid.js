8ea415390cff8dcd9794e61e92bee48ee5fdc832
Asumji <64544378+Asumji@users.noreply.github.com>
2025-02-13 09:32:06 +0100

    Update pyramid.js
    I forgot arrow function exists :fire:

--- pyramid.js
+++ pyramid.js
@@ -1,5 +1,3 @@
-function pyramid(layers) {
-    return ",".repeat(layers-1).split(",").reduce((a,v,i) => a = [...a," ".repeat(layers-i)+"*".repeat(i*2+1)],[]).join("\n")
-}
+const pyramid = (layers) => ",".repeat(layers-1).split(",").reduce((a,v,i) => a = [...a," ".repeat(layers-i)+"*".repeat(i*2+1)],[]).join("\n")
 
-console.log(pyramid(7))
+console.log(pyramid(5))
